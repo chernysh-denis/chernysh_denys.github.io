dbbea1361e593dbbc93d6999690b235ba599bc12
TatyanaPilke <kykla.mc2@gmail.com>
2020-11-24 22:20:09 +0200

    added animation, pictures

--- build/js/main.js
+++ build/js/main.js
@@ -1,15 +1,19 @@
 $(function(){
   $('.nominations__close').on('click', function(){
-    $('.nominations__content').slideToggle();
+    $(this).next($('.nominations__content')).slideToggle();
   });
 
   $('.header__burger').on('click', function(){
     $('.header__menu').slideToggle();
   });
 
-  // $('.nominations__closed').on('click', function(){
-  //   $('.nominations__content').slideToggle();
-  // });
+  $('.header__btn--btn1').hover(function(){
+    $('.header__image-03').toggleClass('active-image');
+  });
+
+  $('.header__btn--btn2').hover(function(){
+    $('.header__image-04').toggleClass('active-image');
+  });
 
   var mixer = mixitup('.juri__foto');
 });
